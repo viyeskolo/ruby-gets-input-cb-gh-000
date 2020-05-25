5b15131bac7ca176f5c41d5b8ebd20e3c68f36f3
Oğuzhan Öztürk <viyeskolo5@gmail.com>
2020-05-25 01:11:12 +0000

    Done.

--- spec/lib/hello_ruby_programmer_spec.rb
+++ spec/lib/hello_ruby_programmer_spec.rb
@@ -1,6 +1,6 @@
 require 'hello_ruby_programmer'
 
-describe 'greeting' do
+def greeting(name)
   it 'accepts an argument called `name`' do
     expect(STDOUT).to receive(:puts).with("Hi, you! Welcome to the wonderful world of Ruby programming.")
 
